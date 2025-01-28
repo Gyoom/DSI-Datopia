7774af690c18ceea95cfb5e9c7718b8f5e9d13a2
PIETTE Guillaume <g.piette@rubika-edu.com>
2025-01-28 14:31:26 +0100

    Add Datopia UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); 
+        rb.velocity = Vector3.zero;
     }
 
     // Update is called once per frame
